b964bed1ce720387e56d619a1e86c37f9799e23f
CaityGossland <caitygossland@gmail.com>
2020-12-15 04:26:41 +0000

    Adds comment/like logic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
 
     belongs_to :user
-    belongs_to :finstagram_posts
-    has_many :comments
-    has_many :likes
+    belongs_to :finstagram_post
+
+    validates_presence_of :text, :user, :finstagram_post
+
 end
